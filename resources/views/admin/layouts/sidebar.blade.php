530a6bbcd1fe6fe83581deb6bde48c8fb7bcf6f9
DevAude <dev.aude.ci@gmail.com>
2026-02-20 10:26:42 +0000

    added admin manage users

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -271,7 +271,7 @@
                                    <span class="nav-icon">
                                         <iconify-icon icon="solar:shop-bold-duotone"></iconify-icon>
                                    </span>
-                                   <span class="nav-text"> Sellers </span>
+                                   <span class="nav-text"> Manage Vendors </span>
                               </a>
                               <div class="collapse" id="sidebarSellers">
                                    <ul class="nav sub-navbar-nav">
@@ -293,6 +293,16 @@
                                    </ul>
                               </div>
                          </li>
+                         <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.manage-user.index') }}" >
+                  <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                  </span>
+                  <span class="nav-link-title">
+                    Manage Users
+                  </span>
+                </a>
+              </li>
 
                          <li class="menu-title mt-2">Other</li>
 
