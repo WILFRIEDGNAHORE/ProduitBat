819f9d96041c567458972df44127586b06fdc669
DevAude <dev.aude.ci@gmail.com>
2026-02-20 14:49:52 +0000

    added frontend design

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,21 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\UserDashboardController;
 use App\Http\Controllers\User\UserProfileController;
 use App\Http\Controllers\User\UserVendorRequest;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\User\UserVendorRequestController;
 use App\Http\Controllers\Vendor\VendorProfileController;
 use App\Http\Controllers\Vendor\VendorDashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::get('vendor/register', [RegisteredVendorController::class, 'create'])->name('vendor.register.index');
 Route::post('vendor/register/store', [RegisteredVendorController::class, 'store'])->name('vendor.register.store');
 
-Route::group(['middleware' => ['auth', 'verified', 'check_role:user'], 'prefix' => 'user', 'as' => 'user.'], function(){
+/**
+ * User Routes
+ */
+Route::group(['middleware' => ['auth', 'verified', 'check_role:user'], 'prefix' => 'user', 'as' => 'user.'], function () {
 
     Route::resource('vendor-request', UserVendorRequestController::class);
     
@@ -26,17 +27,33 @@ Route::group(['middleware' => ['auth', 'verified', 'check_role:user'], 'prefix' 
     Route::resource('profile', UserProfileController::class);
 });
 
-Route::group(['middleware' => ['auth', 'verified', 'check_role:vendor'], 'prefix' => 'vendor', 'as' => 'vendor.'], function(){
+/**
+ * User Routes
+ */
+
+/**
+ * Vendor Routes
+ */
+Route::group(['middleware' => ['auth', 'verified', 'check_role:vendor'], 'prefix' => 'vendor', 'as' => 'vendor.'], function () {
 
     Route::resource('profile', VendorProfileController::class);
     Route::get('dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+/**
+ * Vendor Routes
+ */
+
+
+ /**
+ * Frontend Routes
+ */
+Route::get('/', [HomeController::class, 'home'])->name('home');
+
+  /**
+ * Frontend Routes
+ */
+
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
