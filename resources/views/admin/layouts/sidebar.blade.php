f18baabef98206895fecd9b5acbc2cb1ea667047
WILFRIEDGNAHORE <gnahorewilfried123@gmail.com>
2026-02-25 20:53:36 +0000

    added admin manage category

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -82,18 +82,18 @@
                                    <span class="nav-icon">
                                         <iconify-icon icon="solar:clipboard-list-bold-duotone"></iconify-icon>
                                    </span>
-                                   <span class="nav-text"> Category </span>
+                                   <span class="nav-text"> Manage Categories </span>
                               </a>
                               <div class="collapse" id="sidebarCategory">
                                    <ul class="nav sub-navbar-nav">
                                         <li class="sub-nav-item">
-                                             <a class="sub-nav-link" href="category-list.html">List</a>
+                                             <a class="sub-nav-link" href="{{ route('admin.category.index') }}">Manage Category</a>
                                         </li>                         
                                         <li class="sub-nav-item">
-                                             <a class="sub-nav-link" href="category-edit.html">Edit</a>
+                                             <a class="sub-nav-link" href="category-edit.html">Manage Sub-category</a>
                                         </li>
                                         <li class="sub-nav-item">
-                                             <a class="sub-nav-link" href="category-add.html">Create</a>
+                                             <a class="sub-nav-link" href="category-add.html">Manage Child-category</a>
                                         </li>
                                    </ul>
                               </div>
--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Admin\ManageAdminController;
 use App\Http\Controllers\Admin\VendorRequestController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ApprovedVendorController;
 use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\Admin\AdminProfileController;
@@ -40,6 +41,7 @@ Route::group(['middleware' => 'guest:admin', 'prefix' => 'admin', 'as' => 'admin
 
 Route::group(['middleware' => 'auth:admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
 
+    Route::resource('category', CategoryController::class);
     Route::resource('slider', SliderController::class);
     Route::resource('manage-admin', ManageAdminController::class);
     Route::resource('manage-user', ManageUserController::class);
